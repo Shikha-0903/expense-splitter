715ceb646c69f7e40f05649ad87a932e95529611
Shikha Prajapati <shikha992003@gmail.com>
2026-01-03 10:49:15 +0530

    [FEAT]: Set up initial Flutter project with Supabase, navigation, and environment configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:expense_splitter/src/core/router/routes.dart';
 import 'package:expense_splitter/src/core/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:expense_splitter/src/core/supabase/supabase_client.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  final supabaseManager = SupabaseClientManager();
+  await supabaseManager.initialize();
   runApp(const MyApp());
 }
 
